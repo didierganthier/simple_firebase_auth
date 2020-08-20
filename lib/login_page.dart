09d72022dd4b90a2ac90eb9c0dcb439cf57f0333
didierganthier <didierganthierperan@gmail.com>
2020-08-20 18:27:05 -0400

    LoginPage transformed in StatefulWidget

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Text('Login Page Flutter Firebase'),
+      ),
+      body: Center(
+        child: Text('Login Page Flutter Firebase content'),
+      ),
     );
   }
 }
