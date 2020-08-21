b0ab352a1ccfba02abf8c9a8ca4a9152c6e196ba
didierganthier <didierganthierperan@gmail.com>
2020-08-21 11:39:52 -0400

    Login page designed (basic)

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,7 +14,28 @@ class _LoginPageState extends State<LoginPage> {
       ),
       body: Container(
         padding: EdgeInsets.all(20.0),
-        child: Column(),
+        child: Column(
+          children: [
+            Text(
+              'Login Information',
+              style: TextStyle(fontSize: 20),
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(labelText: 'Email Adress'),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            RaisedButton(
+              onPressed: (){
+
+              },
+              child: Text("LOGIN"),
+            )
+          ],
+        ),
       ),
     );
   }
