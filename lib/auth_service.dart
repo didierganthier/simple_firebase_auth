80f8bb6574f3614345af4cc55498c6472293429b
didierganthier <didierganthierperan@gmail.com>
2020-08-21 14:02:01 -0400

    FirebaseAuth variable created

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 //{}
 
 class AuthService with ChangeNotifier {
+  final _auth = FirebaseAuth.instance;
   var currentUser;
   
   AuthService(){
