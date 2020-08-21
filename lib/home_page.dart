99559c5bdeeedb720436e4d8cb8646bb8cb6f551
didierganthier <didierganthierperan@gmail.com>
2020-08-21 15:03:20 -0400

    home_page.dart designed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   final FirebaseUser currentUser;
@@ -17,8 +18,32 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Home Flutter Firebase'),
       ),
-      body: Center(
-        child: Text('Home Page Flutter Firebase Content'),
+      body: Column(
+        children: [
+          SizedBox(height: 20.0),
+          Text(
+            'Home Page Flutter Firebase  Content',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Text(
+            'Welcome ${widget.currentUser.email}',
+            style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic),
+          ),
+          SizedBox(height: 20.0),
+          RaisedButton(
+            child: Text("LOGOUT"),
+            onPressed: () async {
+              await Provider.of(context).logout();
+            },
+          )
+        ],
       ),
     );
   }
